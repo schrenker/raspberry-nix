2a8ed19f66711d8bb3634a43d0a826d1676aef11
schrenker <sebastian@zawadzki.tech>
2025-01-22 09:11:20 +0100

    add public key

--- configurations/base.nix
+++ configurations/base.nix
@@ -23,6 +23,7 @@
       "$y$j9T$n3O9OwpZhBIdE.EVjrv.z.$aXdFNEAcQOWG3sj9yTm90pUh2bMP5V5wo6uXDDLIhO8";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAG0Q9SO1UHD1lFrUwaZW3S74jHwLuu26WKgUcJqNHNG"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPO4JDlM3XMj7UjdJAHPnQnZ7sLEYRpSufz2mBVhQxqV"
     ];
   };
 
