ca80b190d8045457096f1fc651a7b8acdc3261ec
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-01-23 23:04:39 +0100

    cilium moved to subdirectory + l2announcements configured

--- scripts/install-cilium.sh
+++ scripts/install-cilium.sh
@@ -21,7 +21,7 @@ main() {
     # API_SERVER_IP="$(kubectl cluster-info | grep -oEm 1 '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
     # cilium install --set kubeProxyReplacement=true --set k8sServiceHost="${API_SERVER_IP}" --set k8sServicePort=6443 --set ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16" --set operator.replicas=1
     helm repo add cilium https://helm.cilium.io/
-    helm upgrade --install cilium cilium/cilium --version 1.16.6 --namespace kube-system -f workloads/cilium-values.yaml
+    helm upgrade --install cilium cilium/cilium --version 1.16.6 --namespace kube-system -f workloads/cilium/cilium-values.yaml
 }
 
 main "$@"
