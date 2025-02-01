35be8038c2d21ed34abfaaf13cbbdac872c2d703
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-02-01 11:59:59 +0100

    build all the images

---
+++ scripts/build-all-sd-aarch64.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./build-all-sd-aarch64.sh
+    Build all sd-aarch64 images based flake.nix.
+'
+    exit
+fi
+
+cd "$(git rev-parse --show-toplevel)"
+
+main() {
+    nix flake show --json | jq -r '.nixosConfigurations | keys.[]' | while read -r line; do
+        ./scripts/build-sd-aarch64.sh "$line"
+    done
+
+}
+
+main "$@"
