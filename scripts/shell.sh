b1cbc7b611df006a161c37e39b59f5b1679f7ec4
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-02-08 15:03:44 +0100

    shell.sh

---
+++ scripts/shell.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]] || [[ $# -eq 0 ]]; then
+    echo 'Usage: ./shell.sh NAME
+
+Connect to machine via ssh using its NAME only. Ignore host key checking and known hosts file.
+'
+    exit
+fi
+
+cd "$(git rev-parse --show-toplevel)"
+
+main() {
+    IP=$(nix eval .#nixosConfigurations."$1".config.networking.interfaces.end0.ipv4.addresses --json | jq -r .[0].address)
+
+    ssh nixos@"$IP" -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"
+}
+
+main "$@"
