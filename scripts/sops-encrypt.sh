95b747b125c54020d0cb9ad8aad6a7da9057cb12
schrenker <sebastian@zawadzki.tech>
2025-07-23 13:26:22 +0200

    script to encrypt files with sops

---
+++ scripts/sops-encrypt.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]] || [[ $# -eq 0 ]]; then
+
+    echo 'Usage: ./sops-encrypt.sh FILE
+
+Encrypt a file with sops for cluster use.
+'
+    exit
+fi
+
+cd "$(git rev-parse --show-toplevel)"
+
+main() {
+    ./scripts/setup-sops-key.sh
+
+    sops --encrypt --encrypted-suffix Templates "$1"
+
+}
+
+main "$@"
