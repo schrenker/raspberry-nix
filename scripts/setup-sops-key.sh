f6b9035f52bfb19581ae1a9ac593d2a9ac00bc3f
schrenker <sebastian@zawadzki.tech>
2025-07-23 12:53:06 +0200

    script to setup common encryption key for sops

---
+++ scripts/setup-sops-key.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./setup-sops-key.sh
+
+Setup sops encryption key on the cluster.
+1. If key exists on a cluster, reuse it
+2. If key does not exist on a cluster, reuse local one
+3. If key exists neither on cluster, nor locally, generate a new one
+'
+    exit
+fi
+
+cd "$(git rev-parse --show-toplevel)"
+
+main() {
+    if KEYS=$(kubectl get secret sops-key --namespace sops --output jsonpath='{.data.keys\.txt}' | base64 -d); then
+        echo "$KEYS" >./keys.txt
+    else
+        age-keygen --output ./keys.txt 2>/dev/null || echo "Key exists, not overwriting"
+        kubectl create secret generic sops-key --namespace sops --from-file=./keys.txt
+    fi
+
+    AGEKEY=$(grep 'public' ./keys.txt | awk '{print $4}')
+    cat <<EOF >./.sops.yaml
+---
+creation_rules:
+    - age: >-
+        $AGEKEY
+EOF
+
+}
+
+main "$@"
