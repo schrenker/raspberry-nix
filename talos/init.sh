9e7469124303768054627d45571bf2c4c70f489b
schrenker <sebastian@zawadzki.tech>
2025-10-31 14:58:23 +0100

    fully working cluster bootstrap

--- talos/init.sh
+++ talos/init.sh
@@ -18,39 +18,42 @@ fi
 cd "$(dirname "$0")"
 
 main() {
-    export CONTROL_PLANE_IP="192.168.1.170"
+    which talosctl >/dev/null
+
+    export CPLANE_IP="192.168.1.170"
     export WORKER_IP=("192.168.1.171" "192.168.1.172" "192.168.1.173")
     export CLUSTER_NAME="raspberry"
 
-    echo "CTL:: Generating talos secrets"
+    echo "MSG:: Generating talos secrets"
     talosctl gen secrets -o secrets.yaml
 
-    echo "CTL:: Generating configuration"
-    talosctl gen config --with-secrets secrets.yaml $CLUSTER_NAME https://$CONTROL_PLANE_IP:6443
+    echo "MSG:: Generating configuration"
+    talosctl gen config --with-secrets secrets.yaml $CLUSTER_NAME https://$CPLANE_IP:6443
 
-    echo "CTL:: patching configuration files"
+    echo "MSG:: patching configuration files"
     talosctl machineconfig patch controlplane.yaml --patch @raspberry.yaml --output controlplane.yaml
     talosctl machineconfig patch worker.yaml --patch @raspberry.yaml --output worker.yaml
 
-    echo "CTL:: Applying configuration to Control Plane node"
-    talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file controlplane.yaml
+    echo "MSG:: Applying configuration to Control Plane node"
+    talosctl apply-config --insecure --nodes $CPLANE_IP --file controlplane.yaml
 
-    echo "CTL:: Applying configuration to Worker Nodes"
+    echo "MSG:: Applying configuration to Worker Nodes"
     for ip in "${WORKER_IP[@]}"; do
         talosctl apply-config --insecure --nodes "$ip" --file worker.yaml
         echo "Configuration applied to $ip"
         echo ""
     done
 
-    echo "CTL:: Configuring endpoint"
-    talosctl config endpoint $CONTROL_PLANE_IP
+    echo "MSG:: Configuring endpoint"
+    talosctl config endpoint $CPLANE_IP
 
-    echo "CTL:: Bootstrapping cluster"
-    talosctl bootstrap --nodes $CONTROL_PLANE_IP
+    echo "MSG:: Sleep for 3 minutes for NTP to catch up..."
 
-    echo "CTL:: pulling kubeconfig"
-    talosctl kubeconfig alternative-kubeconfig --nodes $CONTROL_PLANE_IP
+    echo "MSG:: Bootstrapping cluster"
+    talosctl bootstrap --nodes $CPLANE_IP
 
+    echo "MSG:: pulling kubeconfig"
+    talosctl kubeconfig alternative-kubeconfig --nodes $CPLANE_IP
     mv ./alternative-kubeconfig ../.kube/config
 
 }
